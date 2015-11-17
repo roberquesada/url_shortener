e6617817f12298aafecae4e8301104839a8d0199
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-17 16:59:54 +0100

    generate short link

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,15 @@ class Link < ActiveRecord::Base
   end
 
   def self.generate_short_link
+    letters_min = ('a'..'z').to_a
+    letters_may = ('A'..'Z').to_a
+    letters = letters_min + letters_may
+    short_url = ""
 
+    3.times do |letter|
+      short_url += letters[rand(0..50)]
+    end
+
+    short_url
   end
 end
